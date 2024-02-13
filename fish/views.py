0d37be7e5ebaf8885fab775cc649f47be54849ee
project3 <mprasanna015@gmail.com>
2024-02-13 17:40:34 +0530

    first commit

---
+++ fish/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def jamtiger(request):
+    return HttpResponse('<h1><migrat>Hello Hiii i am prasanna</h1></migrat>')
+
+# Create your views here.
